3413875011f861a41d3836930d2a186109cf7164
frafall <frafall@hotmail.com>
2018-01-06 13:26:45 +0100

    Added args for function

--- bin/denon.py
+++ bin/denon.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
+import sys
 import paho.mqtt.publish as mqtt
 from secrets import MQTTSecrets
 
 device='Denon_RC-1047'
-key='KEY_POWER_ON'
+key='KEY_POWER_OFF'
 
 if __name__ == '__main__':
    conf = MQTTSecrets('notify')
    topic='remote/ir/'+device+'/tx'
+
+   try:
+      if(sys.argv[1] == "on" or sys.argv[1] == "1"):
+         key = "KEY_POWER_ON"
+   except IndexError:
+      pass
+
    mqtt.single(topic, key, 
       hostname=conf.server, port=conf.port, client_id="",
       auth={'username': conf.user, 'password': conf.password})
